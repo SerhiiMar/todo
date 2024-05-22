92862e3f5d42f9fb6783eb9ef92b9c355904be9d
Serhii Marynin <serhii.marynin@gmail.com>
2024-05-22 11:36:29 +0300

    Add base views for Tag and Task

--- tasks/views.py
+++ tasks/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import generic
 
-# Create your views here.
+from tasks.models import Tag, Task
+
+
+class TagListView(generic.ListView):
+    model = Tag
+    context_object_name = "tags"
+
+
+class TagCreateView(generic.CreateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagUpdateView(generic.UpdateView):
+    model = Tag
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TagDeleteView(generic.DeleteView):
+    model = Tag
+    success_url = reverse_lazy("tasks:tag-list")
+
+
+class TaskListView(generic.ListView):
+    model = Task
+    context_object_name = "tasks"
+
+
+class TaskCreateView(generic.CreateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskUpdateView(generic.UpdateView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
+
+
+class TaskDeleteView(generic.DeleteView):
+    model = Task
+    fields = "__all__"
+    success_url = reverse_lazy("tasks:task-list")
