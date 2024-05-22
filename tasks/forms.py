153e6b5ee5b1045c866289a6e8b93b25afa83a2e
Serhii Marynin <serhii.marynin@gmail.com>
2024-05-22 14:35:19 +0300

    Add required arguments to some TaskForm fields

--- tasks/forms.py
+++ tasks/forms.py
@@ -5,10 +5,12 @@ from tasks.models import Tag, Task
 
 class TaskForm(forms.ModelForm):
     tags = forms.ModelMultipleChoiceField(
+        required=False,
         queryset=Tag.objects.all(),
         widget=forms.CheckboxSelectMultiple
     )
     deadline = forms.DateTimeField(
+        required=False,
         widget=forms.DateTimeInput(
             attrs={
                 "type": 'datetime-local',
